0ad9fdfba8abdb7faf529a1ad24efae1f2fe1920
Emad Gamati <emadbox@gmail.com>
2020-04-30 02:47:25 +0200

    Update hr_payroll_structure.py

--- l10n_ly_payroll/models/hr_payroll_structure.py
+++ l10n_ly_payroll/models/hr_payroll_structure.py
@@ -12,11 +12,13 @@ from odoo.exceptions import UserError, ValidationError
 
 class HrWorkEntryType(models.Model):
     _inherit = 'hr.work.entry.type'
+    _description = "Update work entry type"
 
     is_overtime = fields.Boolean("Overtime")
 
 class HrPayrollStructureType(models.Model):
     _inherit = 'hr.payroll.structure.type'
+    _description = "Update salary structre model"
 
     show_wage = fields.Boolean("Show Wage Field in Employee Contract")
 
